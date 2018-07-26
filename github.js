67da6bb3977ebcf385c7388142070b66d31fd40a
ventolucci <ventolucci@hotmail.com>
2018-07-26 12:47:35 -0400

    update

---
+++ github.js
@@ -0,0 +1,32 @@
+class Github {
+  constructor() {
+    this.client_id = 'a2aab0c7d0ad7ec0ad44';
+    this.client_secret = '508051b78f2f9e7753fd1498099c94139aa13b65';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
